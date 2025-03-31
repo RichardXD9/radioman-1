1c920127657de323e4d629108def95d3935f6cdd
teste <teste@gmail.com>
2025-03-31 11:13:38 +0100

    finalmente sem bug

--- postcss.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {},  // Make sure you're using the correct plugin here
-    autoprefixer: {},
-  },
-};
